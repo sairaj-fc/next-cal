a1922350e97818567e135e4b6d987c352ed10e94
sairaj <sairaj@findcoder.io>
2022-08-02 17:23:40 +0530

    events page is done

--- pages/api/event.ts
+++ pages/api/event.ts
@@ -2,11 +2,48 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../prisma";
 
+type PostEvent = {
+  title: string;
+  slug: string;
+  description: string;
+  length: string;
+  location: {
+    online: boolean;
+    meet_link: string;
+  };
+  schedule_id: string;
+  timezone?: string;
+  user_id: string;
+};
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const events = await prisma.event.findMany();
+  if (req.method === "POST") {
+    const body = req.body as PostEvent;
 
-  res.status(200).json({ events });
+    const newEvent = await prisma.event.create({
+      data: {
+        title: body.title,
+        slug: body.slug,
+        description: body.description,
+        length: +body.length || 15,
+        locations: body.location,
+        schedule_id: body.schedule_id,
+        time_zone: body.timezone || "Asia/Kolkata",
+        user_id: body.user_id,
+      },
+    });
+
+    return res.json({
+      event: newEvent,
+    });
+  }
+
+  if (req.method === "GET") {
+    const events = await prisma.event.findMany();
+
+    res.status(200).json({ events });
+  }
 }
--- pages/events.tsx
+++ pages/events.tsx
@@ -1,19 +1,32 @@
 import { useGet } from "../comps/useGet";
 import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import axios from "axios";
-import type { Event } from "@prisma/client";
+import type { Availability, Event, Schedule } from "@prisma/client";
 import { Button } from "@comps/Button";
+import { useAuth } from "./_app";
+import Link from "next/link";
 
 const EventsPage = () => {
+  const { user } = useAuth();
   const { data, loading, error } = useGet<Event[]>(async () => {
     const res = await axios.get("/api/event");
     return res.data.events || [];
   });
+  const { data: schedules } = useGet<Schedule[]>(async () => {
+    const res = await axios.get("/api/schedule");
+    return res.data.schedules || [];
+  });
+
   const [event, setEvent] = useState({
     title: "",
     slug: "",
     description: "",
     length: "",
+    location: {
+      online: true,
+      meet_link: "https://meet.google.com/zar-mzni-yjn?authuser=0",
+    },
+    schedule_id: "",
   });
 
   if (error) {
@@ -39,8 +52,36 @@ const EventsPage = () => {
     setEvent({ ...event, [e.target.name]: e.target.value });
   };
 
-  const handleCreateEvent = () => {
-    console.log(event);
+  const handleCreateEvent = async () => {
+    type PostEvent = {
+      title: string;
+      slug: string;
+      description: string;
+      length: string;
+      location: {
+        online: boolean;
+        meet_link: string;
+      };
+      schedule_id: string;
+      timezone?: string;
+      user_id: string;
+    };
+    let newEvent: PostEvent = {
+      title: event.title,
+      slug: event.slug,
+      description: event.description,
+      length: event.length,
+      location: {
+        online: event.location.online,
+        meet_link: event.location.meet_link,
+      },
+      schedule_id: event.schedule_id,
+      user_id: user.user.id,
+    };
+    if (newEvent.schedule_id === "") return;
+
+    const res = await axios.post("/api/event", newEvent);
+    console.log(res);
   };
 
   return (
@@ -50,7 +91,29 @@ const EventsPage = () => {
         {!data || data.length === 0 ? (
           <div className="mt-6 text-center min-h-[200px]">No Events</div>
         ) : (
-          data.map((event) => <div key={event.id}>{event.title}</div>)
+          <div className="mt-4">
+            {data.map((event) => (
+              <Link href={`/events/${event.id}`} key={event.id}>
+                <a className="block hover:bg-gray-100 py-4 px-3">
+                  <div className="flex space-x-2 items-baseline">
+                    <h3 className="text-slate-900 text-xl">{event.title}</h3>
+                    <p className="text-sm">{`${
+                      user.user.username || "sairaj"
+                    }/${event.slug}`}</p>
+                  </div>
+                  {event.description && (
+                    <p className="text-gray-500">{event.description}</p>
+                  )}
+                  <div className="mt-3">
+                    <p className="text-gray-900 text-sm bg-gray-200 rounded-full w-fit px-2 py-0.5">
+                      {" "}
+                      ⏱ {event.length}min
+                    </p>
+                  </div>
+                </a>
+              </Link>
+            ))}
+          </div>
         )}
       </div>
 
@@ -70,7 +133,7 @@ const EventsPage = () => {
           <label>
             Slug
             <div className="flex items-center bg-gray-100 border border-black">
-              <span className=" text-gray-500 h-full px-2 block">/sairaj/</span>
+              <span className=" text-gray-500 h-full px-2">/sairaj/</span>
               <input
                 onChange={handleChange}
                 className="block w-full bg-white border-0"
@@ -99,6 +162,53 @@ const EventsPage = () => {
               value={event.length}
             />
           </label>
+
+          <label>
+            Location
+            <select className="w-full mb-4" name="location" id="location">
+              <option value="gmeet">Google Meet</option>
+            </select>
+          </label>
+
+          <label>
+            Meet Link
+            <input
+              onChange={(e) => {
+                setEvent({
+                  ...event,
+                  location: { ...event.location, meet_link: e.target.value },
+                });
+              }}
+              className="block mb-4 w-full"
+              type="text"
+              name="meet_link"
+              value={event.location.meet_link}
+              placeholder="https://meet.google.com/eoo-zjpn-xik?authuser=0"
+            />
+          </label>
+
+          <label>
+            Availability
+            <select
+              onChange={(e) => {
+                setEvent({
+                  ...event,
+                  schedule_id: e.target.value,
+                });
+              }}
+              className="w-full mb-4"
+              name="availability"
+              id="availability"
+            >
+              <option value=""></option>
+              {schedules?.map((a) => (
+                <option value={a.id} key={a.name}>
+                  {a.name}
+                </option>
+              ))}
+            </select>
+          </label>
+
           <div className="mt-6 block">
             <Button onClick={handleCreateEvent}>Create Event</Button>
           </div>
